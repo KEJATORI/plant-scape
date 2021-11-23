5b4b42cb4cc832ec591d1cdcf66f608b410b364c
KEJATORI <riverakevinjames@gmail.com>
2021-11-24 06:58:49 +0800

    initialized

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.render("index");
+})
+
+wss.on("connection", (ws) => {
+  console.log("connected");
+
+  try {
+    ws.on("message", (data) => {
+
+      const objectData = JSON.parse(data);
+      console.log(objectData);
+
+      wss.clients.forEach((client) => {
+        if (client !== ws) {
+          client.send(JSON.stringify(objectData));
+        }
+      });
+
+    })
+  } catch (error) {
+    console.log(error);
+  }
+
+  ws.on("close", () => {
+    console.log("Client Disconnected");
+  })
+
+})
+
+server.listen(8080, () => console.log("running on port 8080"));
---
+++ public/script.js
@@ -0,0 +1,92 @@
+const humidity = document.querySelector("#humidity");
+const temperature = document.querySelector("#temperature");
+const heatIndex = document.querySelector("#heatIndex");
+const soilMoisture = document.querySelector("#soilMoisture");
+const soilMoistureThreshold = document.querySelector("#soilMoistureThreshold");
+const isAutoWater = document.querySelector("#isAutoWater");
+const isWater = document.querySelector("#isWater");
+const isCover = document.querySelector("#isCover");
+
+const thresholdSub = document.querySelector("#thresholdSub");
+const thresholdAdd = document.querySelector("#thresholdAdd");
+const autoWater = document.querySelector("#autoWater");
+const water = document.querySelector("#water");
+const cover = document.querySelector("#cover");
+
+let sensorData;
+
+const socket = new WebSocket('ws://192.168.1.13:8080');
+socket.addEventListener("open", () => {
+  console.log("Connected To Server");
+});
+
+socket.onmessage = (message) => {
+  sensorData = JSON.parse(message.data);
+  console.log(sensorData);
+  humidity.innerHTML = sensorData.humidity;
+  temperature.innerHTML = sensorData.temperature;
+  heatIndex.innerHTML = Math.round((sensorData.heatIndex + Number.EPSILON) * 100) / 100;
+  soilMoisture.innerHTML = sensorData.soilMoisture;
+  soilMoistureThreshold.innerHTML = sensorData.soilMoistureThreshold;
+  isAutoWater.innerHTML = (sensorData.isAutoWater) ? "Yes" : "No";
+  isWater.innerHTML = (sensorData.isWater) ? "Yes" : "No";
+  isCover.innerHTML = (sensorData.isCover) ? "Yes" : "No";
+
+  console.log(isAutoWater, sensorData.isAutoWater);
+  console.log(isWater, sensorData.isWater);
+  console.log(isCover, sensorData.isCover);
+
+  autoWater.checked = sensorData.isAutoWater;
+  water.checked = sensorData.isWater;
+  cover.checked = sensorData.isCover;
+};
+
+autoWater.addEventListener("click", () => {
+  const data = {
+    soilMoistureThreshold: sensorData.soilMoistureThreshold,
+    isAutoWater: (autoWater.checked) ? true : false,
+    isWater: sensorData.isWater,
+    isCover: sensorData.isCover
+  }
+  socket.send(JSON.stringify(data));
+})
+
+water.addEventListener("click", () => {
+  const data = {
+    soilMoistureThreshold: sensorData.soilMoistureThreshold,
+    isAutoWater: sensorData.isAutoWater,
+    isWater: true,
+    isCover: sensorData.isCover
+  }
+  socket.send(JSON.stringify(data));
+})
+
+cover.addEventListener("click", () => {
+  const data = {
+    soilMoistureThreshold: sensorData.soilMoistureThreshold,
+    isAutoWater: sensorData.isAutoWater,
+    isWater: sensorData.isWater,
+    isCover: (cover.checked) ? true : false,
+  }
+  socket.send(JSON.stringify(data));
+})
+
+thresholdAdd.addEventListener("click", () => {
+  const data = {
+    soilMoistureThreshold: sensorData.soilMoistureThreshold + 1,
+    isAutoWater: sensorData.isAutoWater,
+    isWater: sensorData.isWater,
+    isCover: sensorData.isCover
+  }
+  socket.send(JSON.stringify(data));
+})
+
+thresholdSub.addEventListener("click", () => {
+  const data = {
+    soilMoistureThreshold: sensorData.soilMoistureThreshold - 1,
+    isAutoWater: sensorData.isAutoWater,
+    isWater: sensorData.isWater,
+    isCover: sensorData.isCover
+  }
+  socket.send(JSON.stringify(data));
+})
